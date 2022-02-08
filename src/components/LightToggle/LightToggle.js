42919f6bf67620633478106fb5b9caabdf3c45e4
@abdurrahman0803 <abdurrahman0803@gmail.com>
2022-02-08 23:44:40 +0100

    added theme UI config

---
+++ src/components/LightToggle/LightToggle.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function LightToggle() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="27"
+      height="27"
+      viewBox="0 0 27 27"
+      fill="none"
+    >
+      <circle
+        cx="13.5"
+        cy="13.5"
+        r="12"
+        fill="white"
+        stroke="white"
+        stroke-width="3"
+      />
+    </svg>
+  );
+}
+
+export default LightToggle;
---
+++ src/gatsby-plugin-theme-ui/index.js
@@ -0,0 +1,16 @@
+export default {
+  initialColorModeName: "light",
+  colors: {
+    text: "#333",
+    background: "#fff",
+    primary: "#639",
+    secondary: "#ff6347",
+    modes: {
+      dark: {
+        text: "#fff",
+        background: "#333333",
+        primary: "#0cf",
+      },
+    },
+  },
+};
