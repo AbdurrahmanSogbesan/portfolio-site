78981b86ecf6591d5bed4a55b1a412e2610dd301
@abdurrahman0803 <abdurrahman0803@gmail.com>
2022-02-10 00:06:23 +0100

    added contact page

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,9 +1,14 @@
 import * as React from "react";
 import "./Button.css";
 
-function Button({ text, onClick }) {
+function Button({ text, onClick, onChange, className, disabled }) {
   return (
-    <button onClick={onClick} className="button">
+    <button
+      onClick={onClick}
+      disabled={disabled}
+      onChange={onChange}
+      className={className}
+    >
       {text}
     </button>
   );
